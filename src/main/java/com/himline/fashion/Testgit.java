ff2cc0e0490bcbcd5a2fbfb76e5aaaa16120d5d0
yuvrajhl <yuvraj.brock@himline.com>
2015-10-07 13:29:46 +0530

    Yuvraj - new class created

---
+++ src/main/java/com/himline/fashion/Testgit.java
@@ -0,0 +1,11 @@
+package com.himline.fashion;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Extendedclass ex = new Extendedclass();
+		ex.bar=10;
+	}
+
+}
