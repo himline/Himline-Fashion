ac9a480c8edb42e65b20b2848a2c546570800ab7
chhavi16 <chhavi.sharma@himline.com>
2015-10-07 17:19:01 +0530

    chhavi commit-- Testarray01

---
+++ src/main/java/com/himline/fashion/TestArray01.java
@@ -0,0 +1,21 @@
+package com.himline.fashion;
+
+public class TestArray01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[5];
+		a[0] = 10;
+		a[1] = 6;
+		a[2] = 10;
+		a[3] = 15;
+		a[4] = 19;
+				
+		// value removed
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+
+		}
+	}
+}
