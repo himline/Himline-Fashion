a9f3d096e406e81c677b8dcf4897d3d085014cc3
anshutomar <anshu.tomar@himline.com>
2015-10-07 15:06:45 +0530

    anshu --- value removed

--- src/main/java/com/himline/fashion/TestArray.java
+++ src/main/java/com/himline/fashion/TestArray.java
@@ -3,14 +3,14 @@ package com.himline.fashion;
 public class TestArray {
 
 	public static void main(String[] args) {
-		
+
 		int a[] = new int[5];
 		a[0] = 10;
 		a[1] = 6;
 		a[2] = 8;
-		a[3] = 9;
+		// value removed 
 
-		for (int i=0; i<a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
 	}
