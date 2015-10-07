ba49a16054e0a6e6d3bbc97a49a98b3aa9958f1e
anshutomar <anshu.tomar@himline.com>
2015-10-07 14:48:48 +0530

    anshu ---- testarray commit

---
+++ src/main/java/com/himline/fashion/TestArray.java
@@ -0,0 +1,18 @@
+package com.himline.fashion;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		
+		int a[] = new int[5];
+		a[0] = 10;
+		a[1] = 6;
+		a[2] = 8;
+		a[3] = 9;
+
+		for (int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
