fcc0464ac17021461cdd65652c792ba706d53cab
anshutomar <anshu.tomar@himline.com>
2015-10-08 13:06:52 +0530

    value changed by chhavi

--- src/main/java/com/himline/fashion/TestArray.java
+++ src/main/java/com/himline/fashion/TestArray.java
@@ -8,11 +8,13 @@ public class TestArray {
 		a[0] = 10;
 		a[1] = 6;
 		a[2] = 8;
-		// hello anshu
-		// value removed 
+		
+		// value removed
+		// shdgfjefhre
 
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
+			System.out.println(a4);
 		}
 	}
 
