8d908403c4bdb44c5ef8c412270d20d0514c2625
yuvrajhl <yuvraj.brock@himline.com>
2015-10-20 13:16:26 +0530

    Success - two different methods to read data from database

--- src/main/java/com/himline/fashion/Jdbcconnection.java
+++ src/main/java/com/himline/fashion/Jdbcconnection.java
@@ -8,15 +8,21 @@ public class Jdbcconnection {
 
 	public static void main(String[] args) {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/linux", "root", "VMware1!");
 			Statement st = con.createStatement();
 			String sql = "SELECT * from hello";
 			ResultSet rs = st.executeQuery(sql);
+
 			while (rs.next()) {
-
-				System.out.println("NAME:  "+rs.getString(1) + "    LASTNAME:  " + rs.getString(2));
-
+				System.out.println("----------------Method 1---------------" );
+				System.out.println("NAME:  " + rs.getString(1) + "    LASTNAME:  " + rs.getString(2));
+				
+				System.out.println("----------------Method 2---------------" );
+				String name = rs.getString("NAME");
+				String lastname = rs.getString("lastname");
+				String other = rs.getString("amarjot");
+				System.out.println("NAME:  " + name + "    LASTNAME:  " + lastname + "   other :" + other);
 			}
 			con.close();
 		} catch (Exception ex) {
