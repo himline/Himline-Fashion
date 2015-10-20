c23e3ca1c3bb0961170dacad3a7ef8aaca12ff29
yuvrajhl <yuvraj.brock@himline.com>
2015-10-20 12:08:27 +0530

    Success - database related to sts

--- src/main/java/com/himline/fashion/Jdbcconnection.java
+++ src/main/java/com/himline/fashion/Jdbcconnection.java
@@ -1,24 +1,28 @@
 package com.himline.fashion;
+
 import java.sql.*;
 
 import org.junit.runner.Result;
+
 public class Jdbcconnection {
 
 	public static void main(String[] args) {
-	try{
-	Class.forName("oracle.jdbc.driver.OracleDriver");	
-	Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:3306:xe","root","{VMware1!}");
-	Statement st = con.createStatement();
-	String sql = "Select * from linux.hello";
-	ResultSet rs = st.executeQuery(sql);
-	while(rs.next()){
-		System.out.println(rs.getInt(1)+"        "+rs.getString(2));
-		con.close();
-	}
-	}
-	catch(Exception ex){
-		System.out.println("not found");
-	}
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/linux", "root", "VMware1!");
+			Statement st = con.createStatement();
+			String sql = "SELECT * from hello";
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				// System.out.println(rs.getString("NAME") +"
+				// "+rs.getString("lastname "));
+				// System.out.println(rs.getString(2) );
+				System.out.println(rs.getNString("NAME") + "   " + rs.getObject("lastname") + "" + rs.getObject("NAME"));
+				con.close();
+			}
+		} catch (Exception ex) {
+
+		}
 	}
 
 }
